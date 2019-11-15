513d067a2517157e94cbdd4dd63d515c48cb6e5a
Yegor B <biryuky1@tcnj.edu>
2019-11-14 20:41:44 -0500

    heroku setup 2

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 app.listen(port);
