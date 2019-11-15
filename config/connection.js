1c887d0c40a4376b031f07f8a8548e28806dc3a9
Yegor B <biryuky1@tcnj.edu>
2019-11-14 20:54:11 -0500

    added heroku db host info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'burgerDB'
-});
+var connection
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burgerDB'
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
